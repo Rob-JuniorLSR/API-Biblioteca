29abd848827ae647ab48d5f0dcb8e8fc6d4a1bfc
Robson Junior <regorobson631@gmail.com>
2026-05-30 20:47:55 -0300

    Update biblioteca.sql

--- biblioteca.sql
+++ biblioteca.sql
@@ -1,16 +1,31 @@
+CREATE TABLE autores (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
 CREATE TABLE livros (
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
-    autor VARCHAR(100) NOT NULL,
     genero VARCHAR(50) NOT NULL,
     ano_publicacao INTEGER NOT NULL,
-    quantidade INTEGER NOT NULL
+    quantidade INTEGER NOT NULL,
+    autor_id INTEGER NOT NULL,
+    FOREIGN KEY (autor_id) REFERENCES autores(id)
 );
 
-INSERT INTO livros (titulo, autor, genero, ano_publicacao, quantidade)
+INSERT INTO autores (nome)
 VALUES
-('Dom Casmurro', 'Machado de Assis', 'Romance', 1899, 10),
-('O Cortiço', 'Aluísio Azevedo', 'Romance', 1890, 8),
-('Capitães da Areia', 'Jorge Amado', 'Drama', 1937, 12),
-('Vidas Secas', 'Graciliano Ramos', 'Drama', 1938, 7),
-('Iracema', 'José de Alencar', 'Romance', 1865, 5);
+('Machado de Assis'),
+('Aluísio Azevedo'),
+('Jorge Amado'),
+('Graciliano Ramos'),
+('José de Alencar');
+
+INSERT INTO livros
+(titulo, genero, ano_publicacao, quantidade, autor_id)
+VALUES
+('Dom Casmurro', 'Romance', 1899, 10, 1),
+('O Cortiço', 'Romance', 1890, 8, 2),
+('Capitães da Areia', 'Drama', 1937, 12, 3),
+('Vidas Secas', 'Drama', 1938, 7, 4),
+('Iracema', 'Romance', 1865, 5, 5);
